afd5cebb18d4da630070e4e57632287fcbc3cc50
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-11 10:56:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+import './App.css';
+
+class App extends Component{
+  state = {
+    todos : [
+      {
+        id : 1,
+        title : 'Take out the trash',
+        completed : false
+      },
+      {
+        id : 2,
+        title : 'Excercise',
+        completed : false
+      },
+      {
+        id : 3,
+        title : 'Coding',
+        completed : false
+      }
+    ]
+  };
+
+  //Toggle complete
+  markComplete = (id) => {
+    this.setState({todos: this.state.todos.map((todo) => {
+      if(todo.id === id){
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    })});
+  }
+
+  render(){
+    console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+      </div>
+    );
+  }
+}
+
+export default App;
