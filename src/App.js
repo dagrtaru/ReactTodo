f4d8dc8999cced7c8cce84ef849716188ac4e522
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-11 17:05:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
 import './App.css';
 
 class App extends Component{
@@ -42,7 +44,11 @@ class App extends Component{
     console.log(this.state.todos);
     return (
       <div className="App">
+        <div className="container">
+        <Header />
+        <AddTodo />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }
