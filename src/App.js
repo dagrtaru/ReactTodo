57c378a088372824cc07ef30e5ce91d95a145a56
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-14 17:12:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
@@ -54,13 +55,19 @@ class App extends Component{
   render(){
     console.log(this.state.todos);
     return (
-      <div className="App">
-        <div className="container">
-        <Header />
-        <AddTodo addTodo={this.addTodo}/>
-        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      <Router>
+        <div className="App">
+          <div className="container">
+          <Header />
+          <Route path='/' render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo}/>
+              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+            </React.Fragment>
+          )}/>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
