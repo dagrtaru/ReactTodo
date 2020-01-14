8b94ef6860650abd3aa2de4614afcb0dc4c364a5
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-14 16:46:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import React, { Component } from 'react';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 import './App.css';
 
 class App extends Component{
   state = {
     todos : [
       {
-        id : 1,
+        id : uuid.v4(),
         title : 'Take out the trash',
         completed : false
       },
       {
-        id : 2,
+        id : uuid.v4(),
         title : 'Excercise',
         completed : false
       },
       {
-        id : 3,
+        id : uuid.v4(),
         title : 'Coding',
         completed : false
       }
@@ -43,7 +44,7 @@ class App extends Component{
   //Add Todo
   addTodo = (title) => {
     const newTodo = {
-      id : 4,
+      id : uuid.v4(),
       title : title,
       completed : false
     }
