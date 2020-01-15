6e1fd5b9eb970f246e10275ed97fab1a6a26e44f
Arunarka Mukhopadhyay <35323022+dagrtaru@users.noreply.github.com>
2020-01-15 10:11:00 +0530

    Added axios DELETE request

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ class App extends Component{
 
   //Delete Todo
   delTodo = (id) => {
-    this.setState({ todos : [...this.state.todos.filter(todo => todo.id !== id)] });
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({ todos : [...this.state.todos.filter(todo => todo.id !== id)] }))
   }
 
   //Add Todo
